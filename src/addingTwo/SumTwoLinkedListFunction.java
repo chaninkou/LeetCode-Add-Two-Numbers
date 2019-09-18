174e6eb92b69d081ab8556c31cb28417d78e27a1
chaninkou <michaelkoulove@yahoo.com>
2019-09-17 19:24:05 -0700

    Great problem. Add two numbers in linked list.

--- src/addingTwo/SumTwoLinkedListFunction.java
+++ src/addingTwo/SumTwoLinkedListFunction.java
@@ -2,47 +2,48 @@ package addingTwo;
 
 public class SumTwoLinkedListFunction {
 	public ListNode addTwoNumbers(ListNode list1, ListNode list2) {
+		// Get the reference of both linked list head
 		ListNode l1 = list1;
 		ListNode l2 = list2;
-
+		
+		// solution.next will be the sum of two linked list
 		ListNode solution = new ListNode(0);
-
+		
+		// Make a new pointer of solution
 		ListNode head = solution;
-
+		
 		int sum = 0;
-
-		while (l1 != null || l2 != null) {
-			// If its more than 10, then sum will start from 1 instead of 0
+		
+		while(l1 != null && l2 != null){
+			// sum will be 0 or 1 depend if it is over 10 since over 10 means there is a carry 
 			sum = sum / 10;
-
-			// Add current node to the sum and go to next node
-			if (l1 != null) {
+			
+			// In case both linked list is not same length
+			if(l1 != null){
 				sum = sum + l1.val;
 				l1 = l1.next;
 			}
-
-			// Add current node to the sum and go to next node
-			if (l2 != null) {
+			
+			if(l2 != null){
 				sum = sum + l2.val;
 				l2 = l2.next;
 			}
-
-			// Solution next node will be the sum, mod 10 since it is only one
-			// digit at most
+			
+			// Sum % 10 since if its not a single digit like 12, then add 2 instead 
 			head.next = new ListNode(sum % 10);
-
-			// Make the solution head the same as head.next
+			
+			// Make sure to point at the next element for the next loop
 			head = head.next;
 		}
-
-		// This is for 005 + 005 = 0001
-		// If sum is 10 or more
-		if (sum / 10 == 1) {
-			// Then head.next = 1
+		
+		// If its over 10, that means it is like 0 -> 0 -> 5 + 0 -> 0 -> 5, 
+		// it will be 0 -> 0 -> 0 -> 1
+		if(sum/10 == 1){
+			// Then just next node as 1 since it could only be 1
 			head.next = new ListNode(1);
 		}
-
-		// Since the linked list start from 0, we need to use solution.next
+		
+		// Since we never moved solution pointer, solution.next will be the beginning of the solution
 		return solution.next;
 	}
 }
